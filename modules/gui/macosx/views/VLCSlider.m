402c64d00e2d98ade18974d75e9387e79b404c0b
Gökhan Şimşek <gokhansimsekfb@gmail.com>
2020-10-27 10:38:09 +0300

    Adding frame preview feature

--- modules/gui/macosx/views/VLCSlider.m
+++ modules/gui/macosx/views/VLCSlider.m
@@ -22,6 +22,10 @@
 
 #import "VLCSlider.h"
 
+#import "main/VLCMain.h"
+#import "playlist/VLCPlaylistController.h"
+#import "playlist/VLCPlayerController.h"
+
 #import "extensions/NSView+VLCAdditions.h"
 #import "views/VLCSliderCell.h"
 
@@ -40,8 +44,70 @@
         } else {
             [self setSliderStyleLight];
         }
+
+        /// gvlc
+        // Setup a tracking area when the view is added to the window.
+        NSTrackingArea* trackingArea = [
+            [NSTrackingArea alloc] initWithRect: 
+                [self bounds] 
+                options: (
+                    NSTrackingMouseEnteredAndExited 
+                    | NSTrackingMouseMoved 
+                    | NSTrackingActiveInKeyWindow
+                    | NSTrackingInVisibleRect
+                ) 
+                owner:self 
+                userInfo:nil
+        ];
+        [self addTrackingArea:trackingArea];           
     }
     return self;
+}
+
+- (void) mouseMoved:(NSEvent*)theEvent {
+	NSLog(@"\n --- aaa: mouseMoved");
+
+    NSPoint event_location = theEvent.locationInWindow;
+    NSPoint local_point = [self convertPoint:event_location fromView:nil];
+    int mouseAt = (int)local_point.x;
+    int width = (int)NSWidth(self.bounds);
+    VLCPlayerController * pc = 
+        [[[VLCMain sharedInstance] playlistController] playerController];
+    NSString * name = [pc nameOfCurrentMediaItem];
+    int durSeconds = [pc durationOfCurrentMediaItem]/1000000;
+    int secToShow = (mouseAt * durSeconds)/width + 1;
+    NSString * sec = [NSString stringWithFormat:@"%d", secToShow];
+    NSString * path = @"/Users/gsimsek/Downloads/.";
+    // e.g. /opt/.video-name.mp4/1.jpg
+    NSString * thumbFile = 
+        [NSString stringWithFormat:@"%@%@%@%@%@", path, name, @"/", sec, @".jpg"];
+
+    NSLog(@"--- aaa: mouseAt: %d", mouseAt);
+    NSLog(@"--- aaa: width: %d", width);
+    NSLog(@"--- aaa: name: %@", name);    
+    NSLog(@"--- aaa: total dur: %d secs", durSeconds);
+    NSLog(@"--- aaa: thumbFile: %@", thumbFile);
+    NSLog(@"--- mw: %@", [[NSApplication sharedApplication] mainWindow]);
+
+    NSImage * img = [[NSImage alloc]initWithContentsOfFile:thumbFile];
+    if(img == nil) { NSLog(@" --- aaa: img is nil"); }
+    // NSLog(@" --- aaa: size: %f x %f", img.size.width, img.size.height);
+    // NSMakeRect: x, y, w, h
+    NSRect frame = NSMakeRect(400, 400, 240, (240/img.size.width)*img.size.height);
+    NSWindow* window  = [[NSWindow alloc] initWithContentRect:frame
+                                          styleMask:NSBorderlessWindowMask
+                                          backing:NSBackingStoreBuffered
+                                          defer:NO];
+    [[window contentView] setWantsLayer:YES];
+    [[window contentView] layer].contents = img;
+    [window makeKeyAndOrderFront: NSApp];
+
+    // --- 
+
+}
+
+- (void) mouseEntered:(NSEvent*)theEvent {
+	// NSLog(@"\n --- aaa: mouse entered");
 }
 
 + (Class)cellClass
